bbc5e2abeb76d18adaa73e3cc3639b2f0218d860
Douglas Silva <doug.neo@gmail.com>
2024-05-09 23:23:00 -0300

    Pegando parametros

--- lib/crawler_ar.ex
+++ lib/crawler_ar.ex
@@ -1,7 +1,11 @@
 defmodule CrawlerAr do
   def leveros do
-    Client.produtos()
+    produtos = Client.produtos()
+    |> Map.get("conteudo")
     |> Map.get("produtos")
+
+    produtos
+    # id, nome, estoque, "status", marca-nome, precos-de, precos-por, precos-vista, url
 
   end
 end
