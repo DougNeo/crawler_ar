7bb6d900fb74047221e1a777efc389cdddb6fb5e
Douglas Silva Santos <doug.neo@gmail.com>
2024-05-13 17:13:43 -0300

    Montado as estrutura dos dados pego

--- lib/crawler_ar.ex
+++ lib/crawler_ar.ex
@@ -1,11 +1,20 @@
 defmodule CrawlerAr do
   def leveros do
-    produtos = Client.produtos()
-    |> Map.get("conteudo")
-    |> Map.get("produtos")
+    produtos =
+      Client.produtos()
+      |> Map.get("conteudo")
+      |> Map.get("produtos")
+      |> Enum.map(fn produto ->
+        Map.take(produto, ["id", "nome", "estoque", "status", "url", "marca", "precos"])
+      end)
+      |> Enum.map(fn produto ->
+        Map.update!(produto, "marca", fn marca -> Map.get(marca, "nome") end)
+      end)
+      |> Enum.map(fn produto ->
+        Map.update!(produto, "precos", fn precos -> Map.take(precos, ["de", "por", "vista"]) end)
+      end)
 
     produtos
-    # id, nome, estoque, "status", marca-nome, precos-de, precos-por, precos-vista, url
-
+    # marca-nome, precos-de, precos-por, precos-vista
   end
 end
